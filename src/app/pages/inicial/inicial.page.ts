9c58fe687c255cfb0114f42abc38761cdf62e506
BrayanPe <brayanpenaloza57@gmail.com>
2022-09-29 09:02:13 -0300

    primero comit

--- src/app/pages/inicial/inicial.page.ts
+++ src/app/pages/inicial/inicial.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicial',
   templateUrl: './inicial.page.html',
   styleUrls: ['./inicial.page.scss'],
 })
+
 export class InicialPage implements OnInit {
 
-  constructor() { }
+  constructor(private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
+  async showLoading() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      duration: 3000,
+      spinner: 'circles',
+    });
+
+    loading.present();
+}
 
 }
