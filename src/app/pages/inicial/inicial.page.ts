c0619b8350388bbbcd89a80fe6fc38731962135c
Cristopher Vallejos <cristophervallejos01@gmail.com>
2022-09-29 19:16:34 -0300

    FixRamas-Rama actualizada correctamente

--- src/app/pages/inicial/inicial.page.ts
+++ src/app/pages/inicial/inicial.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-<<<<<<< HEAD
-=======
 import { Router } from '@angular/router';
->>>>>>> brayanPe
 
 @Component({
   selector: 'app-inicial',
@@ -12,20 +8,6 @@ import { Router } from '@angular/router';
 })
 export class InicialPage implements OnInit {
 
-<<<<<<< HEAD
-  constructor(private loadingCtrl: LoadingController) { }
-
-  ngOnInit() {this.showLoading()}
-  async showLoading() {
-    const loading = await this.loadingCtrl.create({
-      message: 'Loading...',
-      duration: 3000,
-      spinner: 'circles',
-    });
-
-    loading.present();
-}
-=======
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -34,7 +16,6 @@ export class InicialPage implements OnInit {
     }, 3000);
  
   }
->>>>>>> brayanPe
 
 
 }
