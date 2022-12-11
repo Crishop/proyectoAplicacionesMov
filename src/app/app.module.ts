1c47388d9c90e578e6e577eea4bee2f423928224
Cristopher Vallejos <cristophervallejos01@gmail.com>
2022-12-11 17:10:35 -0300

    RUN IOS - Enabled - byCris

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,27 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { getApp, initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideAuth,getAuth, initializeAuth, indexedDBLocalPersistence } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage'
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { Capacitor } from '@capacitor/core';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
             provideFirebaseApp(() => initializeApp(environment.firebase)), 
-            provideAuth(() => getAuth()),
+            //provideAuth(() => getAuth()) -No sirve para sistema IOS -ByCris-
+            provideAuth(() => {
+              if (Capacitor.isNativePlatform()) {
+                return initializeAuth(getApp(),{
+                persistence:indexedDBLocalPersistence})
+              }else{
+                return getAuth()
+              }
+            }),
             provideFirestore(() => getFirestore()),
             AuthGuardModule,
             HttpClientModule,
